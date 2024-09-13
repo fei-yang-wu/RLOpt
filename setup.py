4f2352942a774f9ef290ce1d01554d91cd5ec56a
Wu Feiyang <35396618+FeiyangWuPK@users.noreply.github.com>
2024-09-13 09:02:43 -0400

    Add setup.py for proper packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RLOpt',
+    version='0.1.0',
+    description='A reinforcement learning optimization project',
+    author='Feiyang Wu',
+    author_email='feiyangwu@gatech.edu',
+    url='https://github.com/FeiyangWuPK/RLOpt',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'gymnasium',
+        'torch>=2.2',
+        "tensorboard",
+        "tensordict",
+        "tqdm",
+        "rich",
+        "pyyaml",
+        "stable-baselines3",
+        "wandb",
+        "sb3-contrib",
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
