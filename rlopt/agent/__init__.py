f603929935364dbaa2fccb229d3eba37720a028a
Feiyang Wu <feiyangwu@gatech.edu>
2025-01-28 17:36:48 -0500

    Add TeacherStudentLearning to module exports

--- rlopt/agent/__init__.py
+++ rlopt/agent/__init__.py
@@ -1,5 +1,6 @@
 from .l2t.recurrent_l2t import RecurrentL2T
 from .l2t.l2t import L2T
 from .ppo.ppo import PPO
+from .tsl.teacher_student_learning import TeacherStudentLearning
 
-__all__ = ["RecurrentL2T"]
+__all__ = ["RecurrentL2T", "L2T", "PPO", "TeacherStudentLearning"]
